481461b6de15a41da04551c17d34df107b9cec63
DipakDai <dipakkumal673@gmail.com>
2024-01-05 22:57:24 +0545

    design added

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,90 @@
 import React, { ChangeEventHandler } from "react";
-
 import * as Helpers from "./utils/helper.utils";
 import { CustomImage } from "./Components/CustomImage";
+import {
+  Flex,
+  Box,
+  Heading,
+  Text,
+  Button,
+  Input,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
 
 function App() {
   const [uploadedImages, setUploadedImages] = React.useState<CustomImage[]>([]);
 
-  const handleImageUpload = React.useCallback<
-    ChangeEventHandler<HTMLInputElement>
-  >(
-    (event) => {
-      const fileList = event.target.files;
-      const fileArray = fileList ? Array.from(fileList) : [];
-      const fileToImagePromises = fileArray.map(Helpers.fileToImageURL);
+  const handleImageUpload: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const fileList = event.target.files;
+    const fileArray = fileList ? Array.from(fileList) : [];
+    const fileToImagePromises = fileArray.map(Helpers.fileToImageURL);
 
-      Promise.all(fileToImagePromises).then(setUploadedImages);
-    },
-    [setUploadedImages]
-  );
+    Promise.all(fileToImagePromises).then(setUploadedImages);
+  };
 
-  const cleanUpUploadedImages = React.useCallback(() => {
+  const cleanUpUploadedImages = () => {
     setUploadedImages([]);
     uploadedImages.forEach((image) => {
       URL.revokeObjectURL(image.src);
     });
-  }, [setUploadedImages, uploadedImages]);
+  };
 
-  const generatePdfFromImages = React.useCallback(() => {
+  const generatePdfFromImages = () => {
     Helpers.generatePdfFromImages(uploadedImages);
     cleanUpUploadedImages();
-  }, [uploadedImages, cleanUpUploadedImages]);
+  };
 
   return (
-    <>
-      <h1>Convert images to PDFs</h1>
+    <Box>
+      <Flex direction="column" align="center" justify="center" minH="100vh">
+        {/* <Heading mb={4}>Convert images to PDFs</Heading> */}
 
-      <div className="images-container">
-        {uploadedImages.length > 0 ? (
-          uploadedImages.map((image) => (
-            <img key={image.src} src={image.src} className="uploaded-image" width={"100px"} />
-          ))
-        ) : (
-          <p>Upload some images...</p>
-        )}
-      </div>
+        <Wrap spacing={4} justify="center">
+          {uploadedImages.length > 0 ? (
+            uploadedImages.map((image) => (
+              <WrapItem key={image.src}>
+                <img
+                  src={image.src}
+                  width={"100px"} // Set the desired width
+                  height={"100px"} // Set the desired height
+                  alt="Uploaded"
+                  style={{ objectFit: "cover" }} // Optional: maintain aspect ratio
+                />
+              </WrapItem>
+            ))
+          ) : (
+            <Heading>Upload some images...</Heading>
+          )}
+        </Wrap>
+        <Flex direction="column" align="center" mt={4}>
+          <label htmlFor="file-input">
+            <Button as="span" colorScheme="teal" variant="outline">
+              Upload images
+            </Button>
+            <Input
+              id="file-input"
+              type="file"
+              accept="image/*"
+              onChange={handleImageUpload}
+              display="none"
+              multiple
+            />
+          </label>
 
-      <div className="buttons-container">
-        <label htmlFor="file-input">
-          <span className="button">Upload images</span>
-          <input
-            id="file-input"
-            type="file"
-            accept="image/*"
-            onChange={handleImageUpload}
-            style={{ display: "none" }}
-            multiple
-          />
-        </label>
-
-        <button
-          onClick={generatePdfFromImages}
-          className="button"
-          disabled={uploadedImages.length === 0}
-        >
-          Generate PDF
-        </button>
-      </div>
-    </>
+          {uploadedImages.length > 0 && (
+            <Button
+              onClick={generatePdfFromImages}
+              colorScheme="teal"
+              variant="solid"
+              mt={4}
+            >
+              View PDF
+            </Button>
+          )}
+        </Flex>
+      </Flex>
+    </Box>
   );
 }
 
